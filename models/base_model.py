7447a9b9ff7c766b6d7c9117579a51565443db89
Ngwere <ngwerecliford@gmail.com>
2023-06-01 11:53:07 -0700

    hot fixes

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,13 @@
 The BaseModel package
 """
 
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 class BaseModel:
     """Custom base for alll the classes in the AirBnB consol project 
 
-    Arguments:
+    Attributes:
         id(str): handles unique user identity
         created_at: assigns current datetime
         updated_at: updates current datetime
@@ -19,10 +19,26 @@ class BaseModel:
         to_dict(self): returns the dictionary of the model attributes
     """
     def __init__(self, *args, **kwargs):
-        """Initialize BaseModel instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        """Initialize BaseModel instance
+
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attributes values
+
+        """
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        else:
+            for key, value in kwargs.items():
+                if key in ("update_at", "created_at"):
+                    self.__dict__[key] = datetime(value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self._dict__[key] = value
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
@@ -30,7 +46,7 @@ class BaseModel:
 
     def save(self):
         """Update the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of __dict__ of the instance"""
